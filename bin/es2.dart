a38044fb4ba977584cb0acf76896dc3067333ec3
GianITS <gianluca.mattiello.studio@fitstic-edu.com>
2023-02-16 15:20:07 +0100

    Creazione iniziale

---
+++ bin/es2.dart
@@ -0,0 +1,5 @@
+import 'package:es2/es2.dart' as es2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${es2.calculate()}!');
+}
---
+++ test/es2_test.dart
@@ -0,0 +1,8 @@
+import 'package:es2/es2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
